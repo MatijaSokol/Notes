3b952ab7839279b644acab16f30feb93cb602ef0
matija-sokol <matijasokol123@gmail.com>
2024-12-07 13:24:27 +0100

    chore: Rename sharedModule from composeApp module to composeAppSharedModule to prevent conflict with one from shared module

--- composeApp/src/commonMain/kotlin/com/matijasokol/notes/di/ComposeAppSharedModule.kt
+++ composeApp/src/commonMain/kotlin/com/matijasokol/notes/di/ComposeAppSharedModule.kt
@@ -6,6 +6,6 @@ import org.koin.core.module.dsl.singleOf
 import org.koin.dsl.bind
 import org.koin.dsl.module
 
-val sharedModule = module {
+val composeAppSharedModule = module {
     singleOf(::NavigatorImpl) bind Navigator::class
 }
