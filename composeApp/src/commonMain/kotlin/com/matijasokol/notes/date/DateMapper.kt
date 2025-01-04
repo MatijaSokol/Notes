dd05d205a758961d41b260d34f2f92045a63f773
matija-sokol <matijasokol123@gmail.com>
2025-01-04 10:56:11 +0100

    feature: Add viewmodel logic for updating and creating note

--- composeApp/src/commonMain/kotlin/com/matijasokol/notes/date/DateMapper.kt
+++ composeApp/src/commonMain/kotlin/com/matijasokol/notes/date/DateMapper.kt
@@ -1,10 +1,13 @@
 package com.matijasokol.notes.date
 
+import kotlinx.datetime.Clock
+import kotlinx.datetime.DateTimeUnit
 import kotlinx.datetime.Instant
 import kotlinx.datetime.LocalDateTime
 import kotlinx.datetime.TimeZone
 import kotlinx.datetime.format
 import kotlinx.datetime.format.char
+import kotlinx.datetime.plus
 import kotlinx.datetime.toLocalDateTime
 
 val dateTimeFormat = LocalDateTime.Format {
@@ -28,3 +31,11 @@ fun timestampToDateFormatted(
 ): String = Instant.fromEpochMilliseconds(timestamp)
     .toLocalDateTime(timeZone)
     .format(dateTimeFormat)
+
+fun millisNow(
+    dayOffset: Int = 0,
+    clock: Clock = Clock.System,
+    timeZone: TimeZone = TimeZone.currentSystemDefault(),
+): Long = clock.now()
+    .plus(dayOffset, DateTimeUnit.DAY, timeZone)
+    .toEpochMilliseconds()
--- composeApp/src/commonMain/kotlin/com/matijasokol/notes/details/NoteDetailsViewModel.kt
+++ composeApp/src/commonMain/kotlin/com/matijasokol/notes/details/NoteDetailsViewModel.kt
@@ -4,11 +4,14 @@ import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import androidx.navigation.toRoute
+import arrow.core.Either
+import com.matijasokol.notes.date.millisNow
+import com.matijasokol.notes.domain.notes.NotesRepository
+import com.matijasokol.notes.domain.notes.model.Note
 import com.matijasokol.notes.navigation.Destination
 import com.matijasokol.notes.ui.viewmodel.STOP_TIMEOUT_MILLIS
 import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.channels.Channel.Factory.BUFFERED
-import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.combine
@@ -20,6 +23,7 @@ import kotlinx.coroutines.launch
 class NoteDetailsViewModel(
     savedStateHandle: SavedStateHandle,
     private val uiMapper: NoteDetailsUiMapper,
+    private val notesRepository: NotesRepository,
 ) : ViewModel() {
 
     private val _actions = Channel<NoteDetailsAction>(capacity = BUFFERED)
@@ -29,10 +33,12 @@ class NoteDetailsViewModel(
     private val title = savedStateHandle.toRoute<Destination.Details>().title
     private val text = savedStateHandle.toRoute<Destination.Details>().text
 
+    private val titleEdited = MutableStateFlow(title.orEmpty())
     private val textEdited = MutableStateFlow(text.orEmpty())
     private val saveActive = MutableStateFlow(false)
 
     val state = combine(
+        titleEdited,
         textEdited,
         saveActive,
         ::toUiState,
@@ -41,7 +47,7 @@ class NoteDetailsViewModel(
         started = SharingStarted.WhileSubscribed(STOP_TIMEOUT_MILLIS),
         initialValue = NoteDetailsState(
             noteId = noteId,
-            title = title,
+            title = title.orEmpty(),
             text = text.orEmpty(),
         ),
     )
@@ -52,11 +58,13 @@ class NoteDetailsViewModel(
                 _actions.send(NoteDetailsAction.NavigateToList)
             }
             is NoteDetailsEvent.OnTextChanged -> textEdited.update { event.text }
-            NoteDetailsEvent.OnSaveClick -> viewModelScope.launch { handleSaveNote() }
+            is NoteDetailsEvent.OnSaveClick -> viewModelScope.launch { handleSaveNote(event.title, event.text) }
+            is NoteDetailsEvent.OnTitleChanged -> titleEdited.update { event.title }
         }
     }
 
     private fun toUiState(
+        title: String,
         text: String,
         saveActive: Boolean,
     ) = uiMapper.toUiState(
@@ -66,10 +74,41 @@ class NoteDetailsViewModel(
         saveActive = saveActive,
     )
 
-    private suspend fun handleSaveNote() {
+    private suspend fun buildNote(
+        title: String,
+        text: String,
+    ) = when (noteId) {
+        null -> Note(
+            id = "",
+            title = title,
+            text = text,
+            userId = "",
+            createdAt = millisNow(),
+            waitingForUpload = false,
+            waitingForDelete = false,
+        )
+        else -> notesRepository.getNoteById(noteId).getOrNull()
+            ?.copy(title = title, text = text)
+            ?: error("Handle error")
+    }
+
+    private suspend fun handleSaveNote(
+        title: String,
+        text: String,
+    ) {
         saveActive.update { true }
-        delay(500)
+
+        val note = buildNote(title = title, text = text)
+        val result = when (noteId) {
+            null -> notesRepository.create(note)
+            else -> notesRepository.update(note)
+        }
+
         saveActive.update { false }
-        _actions.send(NoteDetailsAction.NavigateToList)
+
+        when (result) {
+            is Either.Left -> Unit // handle error
+            is Either.Right -> _actions.send(NoteDetailsAction.NavigateToList) // handle success
+        }
     }
 }
