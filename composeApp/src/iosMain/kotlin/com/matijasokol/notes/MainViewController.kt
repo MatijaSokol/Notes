0ebf6a55e173707b8cbf71e26bf8be4773c3c740
matija-sokol <matijasokol123@gmail.com>
2026-09-09 20:00:49 +0200

    Fix lint issue

--- composeApp/src/iosMain/kotlin/com/matijasokol/notes/MainViewController.kt
+++ composeApp/src/iosMain/kotlin/com/matijasokol/notes/MainViewController.kt
@@ -3,6 +3,7 @@ package com.matijasokol.notes
 import androidx.compose.ui.window.ComposeUIViewController
 import com.matijasokol.notes.di.initKoin
 
+@Suppress("FunctionName")
 fun MainViewController() = ComposeUIViewController(
     configure = {
         enforceStrictPlistSanityCheck = false
